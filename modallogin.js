7d57e1ecf42ab5ae81653d298641d2eaa73487e1
Oskar Schöldström <public@oxy.fi>
2014-12-30 15:16:42 +0100

    fix csrf issue by refreshing form tokens

--- modallogin.js
+++ modallogin.js
@@ -35,9 +35,69 @@
       });
       // Update the hash when a modal is closed so it doesnt reappear on refresh.
       $(document).on('closed.fndtn.reveal', '[data-reveal]', function () {
-        console.debug('modallogin: remove hash to %s', this.id);
+        console.debug('modallogin: remove hash of %s', this.id);
         window.location.hash = '';
       });
+
+      // Refresh the form id.
+      $(document).one('opened.fndtn.reveal', '[data-reveal]', Drupal.modallogin.refreshForms);
+    }
+  };
+
+  Drupal.modallogin = Drupal.modallogin || {};
+
+  Drupal.modallogin.refreshForms = function () {
+    var refreshDefer = $.Deferred();
+    // Trigger once only.
+    $(document).off('opened.fndtn.reveal', '[data-reveal]', Drupal.modallogin.refreshForms);
+
+    var forms = []
+      , form_ids = [];
+
+    $('.modallogin-modal form')
+      // Refresh all the modallogin forms.
+      .each(function() {
+        var $this = $(this)
+          , build_id = $this.find('[name="form_build_id"]').val()
+          , form_id = $this.find('[name="form_id"]').val();
+
+        forms.push({ form_id: form_id, build_id: build_id });
+        form_ids.push(form_id);
+      })
+      .on('submit', function(e) {
+        var form = this;
+        e.preventDefault();
+
+        if (refreshDefer.state() === 'pending') {
+          console.debug('modallogin: refreshing form %s, delaying submit', form.id);
+        }
+
+        // Do not submit until the forms have been refreshed.
+        refreshDefer.always(function() {
+          form.submit();
+        });
+      });
+
+    console.debug('modallogin: refresh forms: %s', form_ids.join(', '));
+
+    $.post(Drupal.settings.basePath + 'modallogin/refresh_form', { forms: forms })
+      .done(function(data) {
+        for (var i = 0, l = data.length; i < l; i++) {
+          var form = data[i];
+          Drupal.modallogin.injectBuildId(form);
+        }
+        console.debug('modallogin: resolving form refresh');
+      })
+      .always(function() {
+        refreshDefer.resolve();
+      });
+  };
+
+  Drupal.modallogin.injectBuildId = function (form) {
+    var $input = $('#' + form.id).find('[name="form_build_id"]');
+    if ($input.val() !== form.build_id) {
+      $input.val(form.build_id);
+      console.debug('modallogin: refreshed form: %s', form.id);
     }
   };
 }(jQuery));
